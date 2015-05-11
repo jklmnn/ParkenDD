04c2f100759b39b81eb694d257b6f212925dc317
jklmnn <jklmnn@web.de>
2015-05-11 16:27:26 +0200

    Improved default screen for smaller devices.

--- app/src/main/java/de/jkliemann/parkendd/SlotListAdapter.java
+++ app/src/main/java/de/jkliemann/parkendd/SlotListAdapter.java
@@ -2,7 +2,6 @@ package de.jkliemann.parkendd;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.location.Location;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,27 +42,24 @@ public class SlotListAdapter extends ArrayAdapter<ParkingSpot> {
         LayoutInflater inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
         View slotView = inflater.inflate(R.layout.slot_list_adapter, parent, false);
         TextView countView = (TextView)slotView.findViewById(R.id.countView);
-        TextView freeView = (TextView)slotView.findViewById(R.id.freeView);
         TextView nameView = (TextView)slotView.findViewById(R.id.nameView);
         TextView distanceView = (TextView)slotView.findViewById(R.id.distanceView);
         ParkingSpot spot = spots[position];
         nameView.setText(spot.name());
-        countView.setText(context.getString(R.string.available) + " " + Integer.toString(spot.count()));
         if(spot.state().equals(CLOSED)) {
-            freeView.setText(context.getString(R.string.closed));
-            freeView.setTextColor(this.red);
+            countView.setText(context.getString(R.string.closed));
+            nameView.setTextColor(this.red);
         }else if(spot.state().equals(NODATA)){
-            freeView.setText(context.getString(R.string.nodata));
-            freeView.setTextColor(this.blue);
+            countView.setText(context.getString(R.string.nodata) + " (" + Integer.toString(spot.count()) + ")");
+            nameView.setTextColor(this.blue);
         }else{
-            freeView.setTypeface(null, Typeface.BOLD);
-            freeView.setText(Integer.toString(spot.free()));
+            countView.setText(Integer.toString(spot.free()) + " " + context.getString(R.string.available) + " " + Integer.toString(spot.count()));
             if(spot.state().equals(MANY)){
-                freeView.setTextColor(this.green);
+                nameView.setTextColor(this.green);
             }else if(spot.state().equals(FEW)){
-                freeView.setTextColor(this.yellow);
+                nameView.setTextColor(this.yellow);
             }else if(spot.state().equals(FULL)){
-                freeView.setTextColor(this.red);
+                nameView.setTextColor(this.red);
             }
         }
         if(currentLocation != null && spot.location() != null){
